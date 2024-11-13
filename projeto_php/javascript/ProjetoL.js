dc3e27275d7d937b0959f9e92cf11cb6d35c6aa3
João Pedro Pereira <joaopedrojppco@gmail.com>
2024-11-13 08:07:19 -0300

    Update ProjetoL.js

--- projeto_php/javascript/ProjetoL.js
+++ projeto_php/javascript/ProjetoL.js
@@ -1,12 +1,3 @@
-const userLogado = JSON.parse(localStorage.getItem("userLogado"));
-const logado = document.querySelector("#logado");
-logado.innerHTML = `${userLogado.user}`;
-function sair() {
-  localStorage.removeItem("token");
-  localStorage.removeItem("userLogado");
-  window.location.href = "ProjetoL.php";
-}
-
 function entrar() {
   let usuario = document.querySelector('#usuario')
   let userLabel = document.querySelector('#userLabel')
