b1fa327141032c861385c420d9e2706b450191b3
João Pedro Pereira <joaopedrojppco@gmail.com>
2024-11-13 08:11:26 -0300

    Delete projeto_php directory

--- projeto_php/javascript/padrao.js
+++
@@ -1,8 +0,0 @@
-const userLogado = JSON.parse(localStorage.getItem("userLogado"));
-const logado = document.querySelector("#logado");
-logado.innerHTML = `${userLogado.user}`;
-function sair() {
-  localStorage.removeItem("token");
-  localStorage.removeItem("userLogado");
-  window.location.href = "ProjetoL.php";
-}
